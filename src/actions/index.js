bd0149857c52de58ba8ba926034c79ec2271dcc2
John Love <john.love4@gmail.com>
2017-08-11 09:40:37 +0300

    add axios and redux-promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,7 @@
-export function selectBook(book) {
-  //selectBook is an ActionCreator, it needs to return an action,
-  //an object with a type property
+export const FETCH_POSTS = 'FETCH_POSTS';
+
+export function fetchPosts() {
   return {
-    type: 'BOOK_SELECTED',
-    payload: book
+    type: FETCH_POSTS
   };
 }
