fae82e48bffa40518e388fcbe74001e1588042a2
John Love <john.love4@gmail.com>
2017-08-12 06:34:06 +0300

    Scaffold PostsNew component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,19 +1,37 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
   componentDidMount() {
-    this.props.fetchPosts();  
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return  _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      )
+    });
   }
 
   render() {
     return (
       <div>
-        Posts Index
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts };
 }
 
 
